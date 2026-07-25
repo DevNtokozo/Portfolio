89516d6573f64cd64fedf4a14f085868f1fbffe3
Ntokozo Mbuli <ntokozombuliz@gmail.com>
2026-07-25 21:37:33 +0200

    Adjusted daycare

--- src/components/constants/index.js
+++ src/components/constants/index.js
@@ -39,7 +39,7 @@ export const myProjects = [
   
    {
   id: 2,
-  title: "Pinky Hearts Day Care Management System",
+  title: "Pinky Hearts Daycare Management System",
   description:
     "A full-stack daycare management system for managing children, parents, teachers, classrooms, and daily operations.",
 
